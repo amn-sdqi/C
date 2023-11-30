15aca74a284bf29b86b582054a022626087331f2
amn-sdqi <amaanshahid2510@gmail.com>
2023-11-30 21:32:07 +0530

    Days programe

---
+++ C_class/days into years, weeks and days.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int D, y, w, d;
+
+    printf("Enter No of days::");
+    scanf("%d", &D);
+
+    y = D / 365;
+    printf("\nYears:%d", y);
+
+    w = (D % 365) / 7;
+    printf("\nWeeks:%d", w);
+
+    d = D - ((y * 365) + (w * 7));
+    printf("\nDays:%d", d);
+    return 0;
+}
