be9557c782a454fa15a7281e943c54719d9f68cd
amn-sdqi <amaanshahid2510@gmail.com>
2024-01-18 15:12:23 +0530

    a

--- C_class/Prime_no/main.c
+++ C_class/Prime_no/main.c
@@ -1,5 +1,6 @@
+
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdbool.h> //used in isPrime Function to pass boolean values
 
 // A school method based C++ program to check if a
 // number is prime
@@ -12,26 +13,21 @@ bool isPrime(int n)
     // Check from 2 to n-1
     for (int i = 2; i < n; i++)
         if (n % i == 0)
-            return false;
+            return false;   /*if the number is divided by any integer between n-1 it will be no prime*/
 
     return true;
 }
-
-
-
 // Driver Program to test above function
 int main()
 {
-    int N = 11, sum = 0;
-    for (int i = 0; i <= N; i++)
+    int Number = 11, sum = 0;
+    for (int i = 0; i <= Number; i++)
     {
         if (isPrime(i))
         {
             sum += i;
         }
     }
-
     printf("%d is the sum of prime number::", sum);
-
     return 0;
 }
