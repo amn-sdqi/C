819a96611b70eae6d2c5d7c638f5f97d2752dd29
amn-sdqi <amaanshahid2510@gmail.com>
2024-01-25 21:17:21 +0530

    ssa

--- exam questions.c
+++ exam questions.c
@@ -331,52 +331,32 @@
 
 //---------------------------------------------------------------ass QUES 16----------------------------------------------------------------------------
 
-int main()
-{
+// int main()
+// {
 
-    // declaring and initializing 2d array
-    int arr[4][4] = {
-        {
-            1,
-            2,
-            3,
-            4,
-        },
-        {
-            5,
-            6,
-            7,
-            8,
-        },
-        {
-            9,
-            10,
-            11,
-            12,
-        },
-        {13, 14, 15, 16}
-        };
+//     // declaring and initializing 2d array
+// int arr[4][4] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
 
-    printf("2D Array:\n");
-    // printing 2d array
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            printf("%d  ", arr[i][j]);
-        }
-        printf("\n");
-    }
+//     printf("2D Array:\n");
+//     // printing 2d array
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             printf("%d  ", arr[i][j]);
+//         }
+//         printf("\n");
+//     }
 
-    for (int i = 0; i < 4; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            if (i=j)
-            printf("%d  ", arr[i][j]);
-        }
-        printf("\n");
-    }
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             if (i=j)
+//             printf("%d  ", arr[i][j]);
+//         }
+//         printf("\n");
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
